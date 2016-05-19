d171a39f30543c4178c6f150d731215b2c37b2d9
Sergey Piterman <sergeypiterman@gmail.com>
2016-05-18 22:34:51 -0700

    [feat] Add basic server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const express = require('express');
+// const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000; //Add env port later
+
+// app.use(bodyParser.urlencoded)
+
+app.use(express.static(__dirname + "/../client"));
+app.use(express.static(__dirname + "/../node_modules"));
+
+// require('./middleware/middleware.js')(app, express);
+
+app.listen(port);
+console.log('Server listening on ' + port);
